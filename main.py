fc5d3bcad91951bc131dbe829759ce40e7c2fb88
Oleh Liubchenko <91903613+liubquanti@users.noreply.github.com>
2023-06-23 12:29:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import disnake
+from disnake.ext import commands
+
+from config import config
+
+
+class Bot(commands.InteractionBot):
+    async def on_ready(self):
+        activity_text = "Text"
+        activity_type = disnake.ActivityType.playing
+        activity = disnake.Activity(type=activity_type, name=activity_text)
+
+        await self.change_presence(activity=activity)
+
+        print("MidTak: Bot started")
+
+
+def main():
+    bot = Bot()
+    bot.run(config["bot_token"])
+
+
+if __name__ == "__main__":
+    main()
